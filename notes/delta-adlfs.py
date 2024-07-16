bca6ab238f34ebbe19a4628e32d0568a66b38a36
Marco Zocca <marco.zocca@unfoldml.com>
2024-07-16 07:40:05 +0300

    token refresh decorator

--- notes/delta-adlfs.py
+++ notes/delta-adlfs.py
@@ -9,6 +9,31 @@ DEFAULT_AZURE_STORAGE_SCOPE = "https://storage.azure.com/.default"
 credential = DefaultAzureCredential()  # FIXME credential comes from elsewhere
 azure_storage_token = credential.get_token(DEFAULT_AZURE_STORAGE_SCOPE)
 
+
+# # token refresh decorator from https://stackoverflow.com/questions/71734360/python-rerun-code-with-new-token-when-token-has-expired 
+# def authorize_on_expire(func):
+#     def wrapper(token, *args, **kwargs):
+#         try:
+#             result = func(token, *args, **kwargs)
+#         except exceptions.TokenExpiredException as e:
+#             token = ... # token refreshing logic
+#             result = func(token, *args, **kwargs)
+#         finally:
+#             return result
+#     return wrapper
+
+# # to be used as a @ decorator
+def token_refresh(f):
+  def wrapper(token, *args, **kwargs):
+    try:
+      y = f(token, *args, **kwargs)
+    except Exception as e:  # TBD what exception is thrown when a stale token is used?
+      token = credential.get_token(DEFAULT_AZURE_STORAGE_SCOPE)
+      y = f(token, *args, **kwargs)
+    finally:
+      return y
+  return wrapper
+
 try:
   if datetime.fromtimestamp(azure_storage_token.expires_on) <= datetime.now():
     azure_storage_token = credential.get_token(DEFAULT_AZURE_STORAGE_SCOPE)
